0bc829c29e1899ce24bdc277a9bfea9c43ac8218
Lorenzo Cipriani <lorenzo@DESKTOP-L6EDCMR>
2023-10-26 12:13:29 +0200

    updated src to give 3d arrays

--- src/ioH5.f90
+++ src/ioH5.f90
@@ -4,18 +4,11 @@ module ioH5
     use iso_fortran_env, only: RK => real64
 
     implicit none
-    
-    private
-
-    integer, parameter :: compression_level = 9
 
 contains
 
-    subroutine create_hdf5_file(file_name, n, m, l, totL, X, Y, Z, Oin, file_id)
+    subroutine create_hdf5_file(file_name, file_id)
 
-        integer,                   intent(in) :: n, m, l, totL
-        real(RK), dimension(totL), intent(in) :: X, Y, Z, Oin
-      
         character(len=*), intent(in)  :: file_name
         integer(hid_t)  , intent(out) :: file_id
         
@@ -33,25 +26,6 @@ contains
           return
         end if
 
-        ! SAVE THE GRID
-
-        call write_real_1d_array(X, "Yq",      file_id, status)
-        call write_real_1d_array(Y, "logtemp", file_id, status)
-        call write_real_1d_array(Z, "lognb",   file_id, status)
-
-        call write_real_1d_array(Oin, "f_in",  file_id, status)
-
-        ! SAVE SOME PARAMETERS
-
-        call write_integer(n, "n", file_id, status)
-        call write_integer(m, "m", file_id, status)
-        call write_integer(l, "l", file_id, status)
-        
-        ! Check for errors
-        if (status /= 0) then
-            write(*,*) 'Error saving to HDF5 file:', trim(file_name)
-            return
-        end if
     end subroutine create_hdf5_file
 
     subroutine close_hdf5_file(file_id)
@@ -70,7 +44,7 @@ contains
       ! Close the HDF5 library
       call h5close_f(status)
 
-  end subroutine close_hdf5_file
+    end subroutine close_hdf5_file
 
     subroutine write_integer(x, name, id, error)
         
@@ -150,7 +124,7 @@ contains
         ! Create the dataset creation property list, add the gzip
         ! compression filter and set the chunk size.
         call h5pcreate_f(H5P_DATASET_CREATE_F, prop_id, error)
-        call h5pset_deflate_f(prop_id, compression_level, error)
+        call h5pset_deflate_f(prop_id, 9, error)
         call h5pset_chunk_f(prop_id, ndims, chunk, error)
         if (error /= 0) then
           write(*,'("cannot create HDF5 property list",/)')
@@ -192,6 +166,78 @@ contains
           stop
         end if
       
-      end subroutine write_real_1d_array
+    end subroutine write_real_1d_array
+
+    SUBROUTINE write_real_3d_array(x, name, id, error)
+        REAL(RK),   INTENT(IN)  :: x(:,:,:)
+        CHARACTER(*),   INTENT(IN)  :: name
+        INTEGER(HID_T), INTENT(IN)  :: id
+        INTEGER,        INTENT(OUT) :: error
+      
+        INTEGER, PARAMETER :: ndims = 3
+        INTEGER(HSIZE_T)   :: xshape(ndims)
+        INTEGER(HSIZE_T)   :: chunk(ndims)
+        INTEGER(HID_T)     :: dspace_id
+        INTEGER(HID_T)     :: dset_id
+        INTEGER(HID_T)     :: prop_id
+        INTEGER(HID_T)     :: dtype_id
+      
+        xshape = shape(x)
+        chunk = shape(x)
+        dtype_id = H5T_NATIVE_DOUBLE
+      
+        ! Create dataspace
+        CALL H5SCREATE_SIMPLE_F(ndims, xshape, dspace_id, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot create HDF5 dataspace",/)')
+          RETURN
+        ENDIF
+      
+        ! Create the dataset creation property list, add the gzip
+        ! compression filter and set the chunk size.
+        CALL H5PCREATE_F(H5P_DATASET_CREATE_F, prop_id, error)
+        CALL H5PSET_DEFLATE_F(prop_id, 9, error)
+        CALL H5PSET_CHUNK_F(prop_id, ndims, chunk, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot create HDF5 property list",/)')
+          RETURN
+        ENDIF
+      
+        ! Create dataset in file
+        CALL H5DCREATE_F(id, name, dtype_id, dspace_id, dset_id, error, prop_id)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot create HDF5 dataset",/)')
+          RETURN
+        ENDIF
+      
+        ! Write to file
+        CALL H5DWRITE_F(dset_id, dtype_id, x, xshape, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot write to HDF5 file",/)')
+          RETURN
+        ENDIF
+      
+        ! Close property list
+        CALL H5PCLOSE_F(prop_id, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot close HDF5 property list",/)')
+          RETURN
+        ENDIF
+      
+        ! Close dataset
+        CALL H5DCLOSE_F(dset_id, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot close HDF5 dataset",/)')
+          RETURN
+        ENDIF
+      
+        ! Close dataspace
+        CALL H5SCLOSE_F(dspace_id, error)
+        IF (error /= 0) THEN
+          WRITE(*,'("cannot close HDF5 dataspace",/)')
+          RETURN
+        ENDIF
+      
+    END SUBROUTINE write_real_3d_array
     
 end module
--- src/main.f90
+++ src/main.f90
@@ -1,9 +1,9 @@
 program loess3d
 
     use iso_fortran_env, only: RK => real64
-
-    use OMP_LIB
     
+    use OMP_LIB
+
     use ioFunc
     use ioH5
     use mathFunc
@@ -12,7 +12,8 @@ program loess3d
 
     ! Physical parameters
     real(RK) :: f, w
-    real(RK), dimension(:), allocatable :: x, y, z, O, Oout, Wout
+    real(RK), dimension(:), allocatable :: x, y, z, O, LO, LW
+    real(RK), dimension(:,:,:), allocatable :: Oout, Wout, Oin, Xout, Yout, Zout
     integer  :: totL, npoints, n, l, m
     
     real(RK), parameter :: frac = 0.1
@@ -20,7 +21,7 @@ program loess3d
 
     ! Computational parameters
     integer(hid_t) :: file_id
-    integer  :: j, d, Nth, status
+    integer  :: ii, i, j, k, d, Nth, status
 
     integer :: num_args
     character(len=4096), dimension(2) :: args
@@ -42,25 +43,21 @@ program loess3d
     end do
 
     call readParams(args(1), totL, n, l, m, Nth)
-    allocate(x(totL), y(totL), z(totL), O(totL))
+    allocate(x(totL), y(totL), z(totL), O(totL), LO(totL), LW(totL))
     call readData(args(1), totL, x, y, z, O)
 
     npoints = int(ceiling(frac*totL))
     d = 2*(degree**2 + 1)
 
-    allocate(Oout(totL), Wout(totL))
-
-    call create_hdf5_file(args(2), n, m, l, totL, x, y, z, O, file_id)
-
     CALL system_clock(count_rate=rate)
     call SYSTEM_CLOCK(iTimes1)
 
     !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(j, f, w) NUM_THREADS(Nth)
-    do j = 1, totL
+    do ii = 1, totL
 
-        call compute_loess(j, totL, npoints, d, x, y, z, O, f, w)
-        Oout(j) = f
-        Wout(j) = w
+        call compute_loess(ii, totL, npoints, d, x, y, z, O, f, w)
+        LO(ii) = f
+        LW(ii) = w
 
     end do
     !$OMP END PARALLEL DO
@@ -70,10 +67,39 @@ program loess3d
     write(*, *) "Total system runtime:", systemtime, " seconds."
     write(*, *) "System runtime for iteration:", systemtime/totL, " seconds."
     
-    call write_real_1d_array(Oout, "f_out", file_id, status)
-    call close_hdf5_file(file_id)
+    allocate(Oout(n,m,l), Wout(n,m,l), Oin(n,m,l), Xout(n,m,l), Yout(n,m,l), Zout(n,m,l))
 
-    deallocate(x, y, z, O, Oout, Wout)
+    !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ii, i, j, k) NUM_THREADS(Nth)
+    do ii = 1, totL
+        k = mod(ii - 1, l) + 1
+        j = mod((ii - 1 - k + 1) / l, m) + 1
+        i = mod(( (ii-k)/l - (j-1)) / m , n) + 1
+
+        Xout(i,j,k) = x(ii)
+        Yout(i,j,k) = y(ii)
+        Zout(i,j,k) = z(ii)
+        Oin(i,j,k)  = O(ii)
+        Oout(i,j,k) = LO(ii)
+        Wout(i,j,k) = LW(ii)
+    end do
+    !$OMP END PARALLEL DO
+
+    deallocate(x, y, z, O, LO)
+
+!!!! OUTPUT
+    call create_hdf5_file(args(2), file_id)
+    call write_integer(n, "n", file_id, status)
+    call write_integer(m, "m", file_id, status)
+    call write_integer(l, "l", file_id, status)
+    call write_real_3d_array(Xout, "Yq",      file_id, status)
+    call write_real_3d_array(Yout, "logtemp", file_id, status)
+    call write_real_3d_array(Zout, "lognb",   file_id, status)
+    call write_real_3d_array(Oin,  "f_in",    file_id, status)
+    call write_real_3d_array(Oout, "f_out",   file_id, status)
+    call close_hdf5_file(file_id)
+!!! END OUTPUT
+
+    deallocate(Oout, Wout, Xout, Yout, Zout, Oin)
 
     stop
 end program loess3d
